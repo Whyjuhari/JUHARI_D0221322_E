0dadcbb2160dc04d6bc6bbaaa737c1a6665dae52
Whyjuhari <juhariwnly2021@gmail.com>
2022-09-10 12:13:06 +0700

    Upload Tugas Pertama

---
+++ src/tugaspertama/TugasPertama.java
@@ -0,0 +1,27 @@
+package tugaspertama;
+public class TugasPertama {
+    public static void main(String[] args) {
+        // Tugas Pertama
+        // Tipe Data Dalam Java
+        String  Nama    = "Juhari"; // Non primitif
+        String  Nim     = "D0221322";
+        String  Jurusan = "Informatika"; 
+        int     Umur    = 18;
+        char    Kelas   = 'E';  // Primitif char 
+        double  Ipk     = 3.3;  // Primitif bilangan desimal
+        boolean Lulus   = true; // Primitif boolean / logika
+
+        // Menampilkan Nilai Dari Masing-Masing Tipe Data
+        System.out.println();
+        System.out.println("\t=====DATA MAHASISWA=====");
+        System.out.println("\t  Nama    : " + Nama);
+        System.out.println("\t  Nim     : " + Nim);
+        System.out.println("\t  Jurusan : " + Jurusan);
+        System.out.println("\t  Umur    : " + Umur + " Tahun");
+        System.out.println("\t  Kelas   : " + Kelas);
+        System.out.println("\t  Ipk     : " + Ipk);
+        System.out.println("\t  Lulus   : " + Lulus);
+        System.out.println("\t========================");
+        System.out.println();
+    }    
+}
